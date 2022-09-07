f76a721aeaf5acbdb096f99a81a091235f02fdfb
Artem Solovev <85392692+TemaTerbi@users.noreply.github.com>
2022-09-07 11:05:31 +0300

    new methodts

--- src/main/java/com/second/secondPrakt/controllers/CatsController.java
+++ src/main/java/com/second/secondPrakt/controllers/CatsController.java
@@ -1,16 +1,16 @@
 package com.second.secondPrakt.controllers;
 
 import com.second.secondPrakt.models.Cats;
+import com.second.secondPrakt.models.Posts;
 import com.second.secondPrakt.posts.CatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/cats")
@@ -51,5 +51,55 @@ public class CatsController {
         List<Cats> catsList = catsRepository.findByBreedContains(title);
         model.addAttribute("cats", catsList);
         return "Posts/Cats/index";
+    }
+
+    @GetMapping("/{id}")
+    public  String read(@PathVariable("id") Long id, Model model)
+    {
+        ArrayList<Cats> arrayList = new ArrayList<>();
+        Optional<Cats> posts = catsRepository.findById(id);
+        posts.ifPresent(arrayList::add);
+        model.addAttribute("cats", arrayList);
+        return "Posts/Cats/info-cats";
+    }
+
+    @GetMapping("/delete/{id}")
+    public  String delete(@PathVariable("id") Long id, Model model)
+    {
+        catsRepository.deleteById(id);
+        return "redirect:/cats/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public  String edit(@PathVariable("id") Long id, Model model)
+    {
+        if (!catsRepository.existsById(id))
+        {
+            return "redirect:/cats/";
+        }
+        ArrayList<Cats> arrayList = new ArrayList<>();
+        Optional<Cats> posts = catsRepository.findById(id);
+        posts.ifPresent(arrayList::add);
+        model.addAttribute("cats", arrayList);
+        return "Posts/Cats/edit-cats";
+    }
+
+    @PostMapping("/edit/{id}")
+    public  String edit_post(@PathVariable("id") Long id, Model model,
+                             @RequestParam("breed") String breed,
+                             @RequestParam("color") String color,
+                             @RequestParam("colorOfEys") String colorOfEys,
+                             @RequestParam("weight") Double weight,
+                             @RequestParam("height") Double height)
+    {
+        Cats cats = catsRepository.findById(id).orElseThrow();
+
+        cats.setBreed(breed);
+        cats.setColor(color);
+        cats.setColorOfEys(colorOfEys);
+        cats.setWeight(weight);
+        cats.setHeight(height);
+        catsRepository.save(cats);
+        return "redirect:/cats/";
     }
 }
--- src/main/java/com/second/secondPrakt/controllers/PostsController.java
+++ src/main/java/com/second/secondPrakt/controllers/PostsController.java
@@ -5,12 +5,11 @@ import com.second.secondPrakt.posts.PostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/posts")
@@ -51,5 +50,55 @@ public class PostsController {
         List<Posts> postsList = postsRepository.findByTitleContains(title);
         model.addAttribute("posts", postsList);
         return "Posts/index";
+    }
+
+    @GetMapping("/{id}")
+    public  String read(@PathVariable("id") Long id, Model model)
+    {
+        ArrayList<Posts> arrayList = new ArrayList<>();
+        Optional<Posts> posts = postsRepository.findById(id);
+        posts.ifPresent(arrayList::add);
+        model.addAttribute("posts", arrayList);
+        return "Posts/info-posts";
+    }
+
+    @GetMapping("/delete/{id}")
+    public  String delete(@PathVariable("id") Long id, Model model)
+    {
+        postsRepository.deleteById(id);
+        return "redirect:/posts/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public  String edit(@PathVariable("id") Long id, Model model)
+    {
+        if (!postsRepository.existsById(id))
+        {
+            return "redirect:/posts/";
+        }
+        ArrayList<Posts> arrayList = new ArrayList<>();
+        Optional<Posts> posts = postsRepository.findById(id);
+        posts.ifPresent(arrayList::add);
+        model.addAttribute("posts", arrayList);
+        return "Posts/edit-posts";
+    }
+
+    @PostMapping("/edit/{id}")
+    public  String edit_post(@PathVariable("id") Long id, Model model,
+    @RequestParam("title") String title,
+    @RequestParam("phone") String phone,
+    @RequestParam("description") String description,
+    @RequestParam("place") String place,
+    @RequestParam("views") Integer views)
+    {
+        Posts posts = postsRepository.findById(id).orElseThrow();
+
+        posts.setTitle(title);
+        posts.setPhone(phone);
+        posts.setDescription(description);
+        posts.setPlace(place);
+        posts.setViews(views);
+        postsRepository.save(posts);
+        return "redirect:/posts/";
     }
 }
